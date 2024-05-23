5b609296d6b27eb5b2fcbfd60e28ff901962d77b
MuxaJlbl417 <sudakov170389@gmail.com>
2024-05-23 19:36:17 +0500

    Initial commit

---
+++ PythonVariable.py
@@ -0,0 +1,5 @@
+summ_h_a = 12
+summ_hour = 1.5
+name = 'Python'
+hour_h_a = summ_hour / summ_h_a
+print('Курс:', name, 'всего задач:', summ_h_a, 'затрачено часов:', summ_hour, 'среднее время выполнения:', hour_h_a)
